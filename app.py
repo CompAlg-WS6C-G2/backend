7ee3eaf99d1fb4c94ca92f05b2ffd9c089dc0dc0
lecav <lecav30@outlook.com>
2023-06-05 21:00:45 -0500

    edges to JSON & 1500 nodes

--- app.py
+++ app.py
@@ -18,28 +18,48 @@ client = MongoClient(
     'mongodb+srv://lepian:tCJRUEhEhRy5YXma@compalg-ws6c-g2.uwf4hxj.mongodb.net/?retryWrites=true&w=majority')
 db = client['data']
 
+# Acceder a los datos de las películas y series
 nodes = list(db['film'].find())
-for node in nodes:
-    node['Title'] = node['Title'].replace(':', ' ')
 
+# Crear grafo
 netflix_graph = nx.Graph()
 
-for e, node in enumerate(nodes):
-    for i in range(e + 1, 21):
-        target = nodes[i]
-        weight = calculate_weight(node, target)
-        if weight > 0:
-            netflix_graph.add_edge(str(node['Title']), str(
-                target['Title']), weight=weight)
-        else:
-            netflix_graph.add_node(str(node['Title']))
-    if e == 20:
-        break
+# Agregar nodos
+for node in nodes:
+    # Eliminar el caracter ':' de los títulos de las películas y series
+    # para evitar problemas con el algoritmo de Dijkstra
+    netflix_graph.add_node(str(node['Title'].replace(':', ' ')))
+
+# Agregar ejes
+try:
+    # Si existe el archivo, agregar los ejes
+    with open('edges.json', 'r') as file:
+        edges = json.load(file)
+        for edge in edges:
+            netflix_graph.add_edge(
+                edge['source'], edge['target'], weight=edge['weight'])
+except FileNotFoundError:
+    # Si no existe, crear el archivo y agregar los ejes
+    edges = []
+    for e, source in enumerate(nodes):
+        for i in range(e + 1, len(nodes)):
+            target = nodes[i]
+            weight = calculate_weight(source, target)
+            if weight > 0:
+                edges.append({
+                    'source': str(source['Title']),
+                    'target': str(target['Title']),
+                    'weight': weight
+                })
+                netflix_graph.add_edge(str(source['Title']), str(
+                    target['Title']), weight=weight)
+    with open('edges.json', 'w') as file:
+        json.dump(edges, file, indent=4)
 
 lst_nodes_graph = list(netflix_graph.nodes)
 
 
-@app.route('/')
+@ app.route('/')
 def home():
     """
     Lista de los nombres de las películas y series
@@ -47,7 +67,7 @@ def home():
     return lst_nodes_graph
 
 
-@app.route('/links')
+@ app.route('/links')
 def graph_links():
     """
     Aristas del grafo
@@ -55,7 +75,7 @@ def graph_links():
     return jsonify(nx.node_link_data(netflix_graph).get('links'))
 
 
-@app.route('/nodes')
+@ app.route('/nodes')
 def graph_nodes():
     """
     Nodos del grafo
@@ -63,7 +83,7 @@ def graph_nodes():
     return jsonify(nx.node_link_data(netflix_graph).get('nodes'))
 
 
-@app.route('/dijkstra/<string:start>/<string:end>')
+@ app.route('/dijkstra/<string:start>/<string:end>')
 def dijkstra_route(start: str, end: str):
     """
     Camino mínimo entre dos nodos con el Algortimo Dijkstra
@@ -79,7 +99,7 @@ def dijkstra_route(start: str, end: str):
     return json.dumps({'path': path, 'distance': dist}, default=json_util.default)
 
 
-@app.route('/mongodb_nodes')
+@ app.route('/mongodb_nodes')
 def mongodb_data():
     """
     Datos de las películas y series en MongoDB
@@ -87,7 +107,7 @@ def mongodb_data():
     return json.dumps({'nodes': nodes}, default=json_util.default)
 
 
-@app.route('/mongodb_nodes/<string:title>')
+@ app.route('/mongodb_nodes/<string:title>')
 def mongodb_data_by_title(title: str):
     """
     Película o serie en MongoDB por título
